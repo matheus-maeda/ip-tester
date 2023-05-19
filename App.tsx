30f72688c92523feda5503130aaf685a09c159f7
matheus-maeda <79715828+matheus-maeda@users.noreply.github.com>
2023-05-19 15:32:28 -0300

    3 files modified

--- App.tsx
+++ App.tsx
@@ -3,27 +3,34 @@ import './style.css';
 
 export default function App() {
   const [message, setMessage] = React.useState('');
-
+  const [error, setError] = React.useState(' ');
   const handleChange = (event) => {
     setMessage(event.target.value);
   };
   function handleClick() {
-    var urlhttps = 'https://' + message;
-    window.open(urlhttps, '_blank');
-    urlhttps += ':8082';
-    window.open(urlhttps, '_blank');
-    var urlhttp = 'http://' + message;
-    window.open(urlhttp, '_blank');
-    urlhttp += ':8082';
-    window.open(urlhttp, '_blank');
+    try {
+      var urlhttps = 'https://' + message;
+      window.open(urlhttps, '_blank');
+      urlhttps += ':8082';
+      window.open(urlhttps, '_blank');
+      var urlhttp = 'http://' + message;
+      window.open(urlhttp, '_blank');
+      urlhttp += ':8082';
+      window.open(urlhttp, '_blank');
+    } catch (e) {
+      setError('Insira um endereço IP válido');
+    }
   }
   return (
     <div className="Menu">
-      <h1>Acessar remotamente roteadores</h1>
-      <p>
-        Insira abaixo o Last-IP que será aberto no seu roteador as abas
-        possíveis para acessar o roteador do cliente
-      </p>
+      <div className="Header">
+        <h1>Acessar remotamente roteadores</h1>
+        <p>
+          Insira abaixo o Last-IP que será aberto no seu roteador as abas
+          possíveis para acessar o roteador do cliente
+        </p>
+        <p id="errorLabel">{error}</p>
+      </div>
       <div className="inputs">
         <input
           type="text"
@@ -34,6 +41,13 @@ export default function App() {
         <button onClick={handleClick} id="sendButton" className="input">
           Tentar IP
         </button>
+      </div>
+      <div className="errors">
+        <p>
+          OBS: Caso seja a primeira vez utilizando, seu navegador pode bloquear
+          a quantidade de abas que será aberta, caso isso aconteça, basta
+          liberar os pop-ups!
+        </p>
       </div>
     </div>
   );
